e2c0ae9541d0db6b056885949ce871bfbdd99aad
jagadeesh kumar <jagadeesh.kumar@bolt.earth>
2023-07-22 12:13:59 +0530

    changes have done in 2nd episode

--- App.js
+++ App.js
@@ -1,17 +1,6 @@
-// const heading = React.createElement(
-//     "h1",{id:"heading",xyz:"abc"},
-//     "Hello Jagadeesh"
-// );
-// console.log(heading)
-// const root=ReactDOM.createRoot(document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-// root.render(heading);
-{/* <div id="parent">
-    <div id="child">
-        <h1>I'm h1 tag</h1>
-        <h1>I'm h2 tag</h1>
-    </div>
-</div> */}
 const parent = React.createElement("div",{id:'parent'},[
     React.createElement("div",{id:"child"},[
         React.createElement("h2",{},"I'm H2 Tag"),
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-#namaste React 
+# namaste React 
+
+# parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - Supports older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
